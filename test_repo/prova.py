b806679778d656b70d3a84c8cd8fd3157f318deb
lorenzomarini96 <lorenzo.marini.1996@gmail.com>
2022-07-07 21:09:44 +0200

    Aggiunta funzione

--- test_repo/prova.py
+++ test_repo/prova.py
@@ -1,11 +1,22 @@
-"""Funzione che dice ciao!"""
+"""Funzioni per socializzare."""
 
 
 
 def hello(nome):
-    """Funzione che dice ciao
+    """Funzione che dice ciao.
 
     Args:
         nome (str): Nome che viene salutato
     """
     print(f'Ciao {nome}')
+
+
+def fuck(nome):
+    """Funzione che manda a quel paese.
+
+    Args:
+        nome (str): Nome del tipo che detesti.
+    """
+    print(f'Fuck you {nome}')
+
+
